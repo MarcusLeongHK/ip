d8959974e3c13e0bb7189ce2dfa35666a88ff472
Marcus <marcusleonghoykitt@gmail.com>
2025-08-22 00:11:15 +0800

    Add input and exit functionality

--- src/main/java/Jordan.java
+++ src/main/java/Jordan.java
@@ -1,8 +1,19 @@
+import java.util.Scanner;
+
 public class Jordan {
     public static void main(String[] args) {
-        String text = "Hello! I'm Jordan!\n"
-                + "How can I help you?\n"
-                + "Bye! See you again!\n";
-        System.out.println(text);
+        String introText = "Hello! I'm Jordan!\n"
+                + "How can I help you?\n";
+        System.out.println(introText);
+        Scanner scanner = new Scanner(System.in);
+        boolean isEcho = true;
+        while (isEcho){
+            String phrase = scanner.next();
+            if (phrase.equals("bye")){
+                isEcho = false;
+            }
+            System.out.println(phrase);
+        }
+        System.out.println("Bye! See you again!\n");
     }
 }
