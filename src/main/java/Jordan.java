ddc09b387f55076583d9f18bb911a0da8f763642
Marcus <marcusleonghoykitt@gmail.com>
2025-08-21 23:56:00 +0800

    Change name of chatbot to Jordan

---
+++ src/main/java/Jordan.java
@@ -0,0 +1,8 @@
+public class Jordan {
+    public static void main(String[] args) {
+        String text = "Hello! I'm Jordan!\n"
+                + "How can I help you?\n"
+                + "Bye! See you again!\n";
+        System.out.println(text);
+    }
+}
