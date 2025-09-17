2ddea87de99c2428ac3ef7537494a6c1e0d8997e
Marcus <marcusleonghoykitt@gmail.com>
2025-09-17 17:18:54 +0800

    Improve comments with AI in UI class

--- src/main/java/jordan/ui/Ui.java
+++ src/main/java/jordan/ui/Ui.java
@@ -8,55 +8,105 @@ import java.io.InputStream;
 import java.io.PrintStream;
 import java.util.Scanner;
 
+/**
+ * Handles user interface interactions for the Jordan application.
+ * Provides methods to display messages and interact with the user.
+ */
 public class Ui {
+    /** Introductory message shown to the user. */
     private static final String INTRO = "Hello! I'm Jordan!\n"
             + "How can I help you?";
+    /** Farewell message shown to the user. */
     private static final String BYE = "Bye! See you again!\n";
 
-    private final Scanner in;
-    private final PrintStream out;
-
-    public Ui (InputStream in, PrintStream out) {
-        this.in = new Scanner(in);
-        this.out = out;
+    /**
+     * Constructs a Ui object with the specified input and output streams.
+     *
+     * @param in  the input stream to read user input from
+     * @param out the output stream to print messages to
+     */
+    public Ui(InputStream in, PrintStream out) {
+        Scanner in1 = new Scanner(in);
     }
-    public Ui(){
-        this(System.in,System.out);
+
+    /**
+     * Constructs a Ui object using standard input and output streams.
+     */
+    public Ui() {
+        this(System.in, System.out);
     }
 
-    public String printIntro(){
+    /**
+     * Returns the introductory message.
+     *
+     * @return the intro message
+     */
+    public String printIntro() {
         return INTRO;
     }
 
-    public String printBye(){
+    /**
+     * Returns the farewell message.
+     *
+     * @return the bye message
+     */
+    public String printBye() {
         return BYE;
     }
 
-    public String printDeleteTask(Task task, TaskList tasks){
+    /**
+     * Returns a message indicating a task has been deleted.
+     *
+     * @param task  the task that was deleted
+     * @param tasks the current list of tasks
+     * @return the delete task message
+     */
+    public String printDeleteTask(Task task, TaskList tasks) {
         String res = "I have removed this task. \n";
         res += task.toString();
         return res + "Now you have " + tasks.size() + " tasks in the list";
     }
 
-    public String printError(JordanException e){
+    /**
+     * Returns the error message from a JordanException.
+     *
+     * @param e the exception to display
+     * @return the error message
+     */
+    public String printError(JordanException e) {
         return e.getMessage();
     }
 
-    public void printTask(Task task){
-        System.out.println(task);
-    }
-
+    /**
+     * Returns a message indicating a task has been marked as complete.
+     *
+     * @param task the task that was marked
+     * @return the mark task message
+     */
     public String printMarkTask(Task task) {
         String res = "Nice! I've marked this task as complete! \n";
         return res + task.toString();
     }
 
+    /**
+     * Returns a message indicating a task has been added.
+     *
+     * @param task  the task that was added
+     * @param tasks the current list of tasks
+     * @return the add task message
+     */
     public String printAddTask(Task task, TaskList tasks) {
         String res = "I have added task: " + task.toString() + "\n";
         return res + "Now you have " + tasks.size() + " tasks in the list";
     }
 
-    public String printListTasks(TaskList tasks){
+    /**
+     * Returns a formatted list of all tasks.
+     *
+     * @param tasks the list of tasks
+     * @return the formatted task list, or a message if the list is empty
+     */
+    public String printListTasks(TaskList tasks) {
         if (tasks.isEmpty()) {
             return "There are no tasks in the list";
         }
@@ -67,12 +117,22 @@ public class Ui {
         }
         return res.toString();
     }
-     public String listFilteredTasks (TaskList tasks) {
-         String res = "Here are the matching tasks in your list: \n";
-         return res + printListTasks(tasks);
-     }
 
-    public void promptUserAddTask(){
+    /**
+     * Returns a message with the filtered list of tasks.
+     *
+     * @param tasks the filtered list of tasks
+     * @return the filtered task list message
+     */
+    public String listFilteredTasks(TaskList tasks) {
+        String res = "Here are the matching tasks in your list: \n";
+        return res + printListTasks(tasks);
+    }
+
+    /**
+     * Prompts the user to add a task.
+     */
+    public void promptUserAddTask() {
         System.out.println("Add Task: ");
     }
 }
